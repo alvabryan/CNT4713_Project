10bfae166dc749ceb706f3e093324f1458212f2b
Bryan Alvarenga <alvarengabryan@hotmail.com>
2022-07-16 11:04:46 -0400

    new update

--- app.py
+++ app.py
@@ -3,19 +3,19 @@ from flask import Flask, render_template, Response
 import cv2
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(0)
+# camera = cv2.VideoCapture(0)
 
-def generate_frames():
-    while True:
-        # read the camera frame
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-        yield(b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+# def generate_frames():
+#     while True:
+#         # read the camera frame
+#         success, frame = camera.read()
+#         if not success:
+#             break
+#         else:
+#             ret, buffer = cv2.imencode('.jpg', frame)
+#             frame = buffer.tobytes()
+#         yield(b'--frame\r\n'
+#                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route("/")
@@ -24,10 +24,10 @@ def home():
     return render_template('home.html')
 
 
-@app.route("/video")
-def video():
-    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
-    # return render_template('video.html')
+# @app.route("/video")
+# def video():
+#     return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+#     # return render_template('video.html')
 
 
 if __name__ == '__main__':
