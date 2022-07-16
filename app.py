49d396ec573e4298e75eb249c2c6fd364c7b2c5c
Bryan Alvarenga <alvarengabryan@hotmail.com>
2022-07-15 23:16:36 -0400

    live feed

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
 from flask import Flask, render_template, url_for
+from flask import Flask, render_template, Response
+import cv2
 app = Flask(__name__)
 
+camera = cv2.VideoCapture(0)
+
+
+def generate_frames():
+    while True:
+
+        # read the camera frame
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+        yield(b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
 @app.route("/")
-@app.route("/login")
-def login():
-    return render_template('login.html')
-
-@app.route("/signup")
-def signup():
-    return render_template('signup.html')
-
 @app.route("/home")
 def home():
     return render_template('home.html')
+
+
+@app.route("/video")
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+    # return render_template('video.html')
 
 
 if __name__ == '__main__':
