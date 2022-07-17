ede92861318169e82ed7e55cc3488f5f249e2944
Bryan Alvarenga <alvarengabryan@hotmail.com>
2022-07-17 16:39:28 -0400

    most recent update

--- app.py
+++ app.py
@@ -1,34 +1,15 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, send_from_directory, url_for
 from flask import Flask, render_template, Response
-import cv2
-app = Flask(__name__)
-
-camera = cv2.VideoCapture()
-
-def generate_frames():
-    while True:
-        # read the camera frame
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-        yield(b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
+app = Flask(__name__, static_folder='pdf')
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
 
-
-@app.route("/video")
-def video():
-    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
-    # return render_template('video.html')
-
+@app.route('/pdf/<path:filename>', methods=['GET', 'POST'])
+def download(filename):    
+    return send_from_directory(directory='pdf', filename=filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
